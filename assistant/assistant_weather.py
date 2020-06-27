f410f0553f1464f63e222515aef533bb8239b02a
innovatore <67472839+innovatore@users.noreply.github.com>
2020-06-27 14:18:54 +0200

    Deleted unnecessary comments

--- assistant/assistant_weather.py
+++ assistant/assistant_weather.py
@@ -25,12 +25,10 @@ def PlayTimeAndWeather(g, l, t):
         cancelWeather = True
         assistant_soundprocess.CleanUpSoundProcessList()
         PlaySystemTime()
-#        PlayWeather()
         
 def PlaySystemTime():
     assistant_soundprocess.CreateSoundProcessAplay(gong, 100)
     locale.setlocale(locale.LC_ALL, "de_DE.UTF-8")
-#    outputstr = "Bei Gongschlag war die Systemzeit " + str(datetime.datetime.now().strftime("%A, der %-d. %b %Y %H:%M:%S."))
     outputstr = "Es ist " + datetime.datetime.now().strftime("%H:%M")
     outputstr = outputstr + " " + choice(weatherString)
     assistant_soundprocess.CreateSoundProcessAplay(assistant_soundprocess.CreateVoiceOut(outputstr), 100)
@@ -46,8 +44,6 @@ def LoadWeatherData(weatherString = weatherString):
         print ("PlayWeather start")
         
         locale.setlocale(locale.LC_ALL, "de_DE.UTF-8")
-#        outputstr = "Bei Gongschlag war die Systemzeit " + str(datetime.datetime.now().strftime("%A, der %-d. %b %Y %H:%M:%S."))
-#        assistant_soundprocess.CreateSoundProcessAplay(assistant_soundprocess.CreateVoiceOut(outputstr), 100)
         proc = subprocess.Popen(["curl ifconfig.me"], stdout=subprocess.PIPE, shell=True)
         (out, err) = proc.communicate()
         print ("program output:", out)
@@ -80,39 +76,11 @@ def LoadWeatherData(weatherString = weatherString):
                 outputstr = outputstr + "{}".format(str(WeatherData["weather"]).split(": ")[3].split(",")[0])
             except KeyError:
                 outputstr = outputstr
-#            try:            
-#                weatherString.append(outputstr + "ist die  Luftfeuchtigkeit: {}%.".format(WeatherData["main"]["humidity"]))
-#            except KeyError:
-#                outputstr = outputstr
-#            try:              
-#                weatherString.append(outputstr + "ist der Luftdruck: {} Hecktopaskal.".format(WeatherData["main"]["pressure"]))
-#            except KeyError:
-#                outputstr = outputstr
             print(format(WeatherData["main"]["temp"],'n') + " Hallo " + locale.format("%.2f",12000,1))
             try:
                 weatherString.append(outputstr + ", bei {} Grad Celsius.".format(format(WeatherData["main"]["temp"], 'n')))
             except KeyError:
                 outputstr = outputstr
-#            try:
-#                weatherString.append(outputstr + " Maximale Temperatur: {} Grad Celsius.".format(format(WeatherData["main"]["temp_max"], 'n')))
-#            except KeyError:
-#                outputstr = outputstr
-#            try:
-#                weatherString.append(outputstr + " Minimale Temperatur: {} Grad Celsius.".format(format(WeatherData["main"]["temp_min"], 'n')))
-#            except KeyError:
-#                outputstr = outputstr
-#            try:
-#                weatherString.append(outputstr + "ist die  Windgeschwindigkeit: {} Meter pro Sekunde.".format(format(WeatherData["wind"]["speed"], 'n')))
-#            except KeyError:
-#                outputstr = outputstr
-#            try:
-#                weatherString.append(outputstr + "ist die Windrichtung: {} Grad.".format(WeatherData["wind"]["deg"]))
-#            except KeyError:
-#                outputstr = outputstr
-#            try:
-#                weatherString.append(outputstr + " ist der Himmel zu {} Prozent bedeckt.".format(WeatherData["clouds"]["all"]))
-#            except KeyError:
-#                outputstr = outputstr
         except ValueError:
             outputstr = "Kein Netzwerk"
             weatherString.append(outputstr)
@@ -125,7 +93,6 @@ def LoadWeatherData(weatherString = weatherString):
             outputstr = "Wetterdaten erfolgreich geladen."
         return assistant_soundprocess.CreateSoundProcessAplay(assistant_soundprocess.CreateVoiceOut(outputstr), 100)
     except IOError:
-#       outputstr = "Kontakt numero 12 geschlossen, Fehler beim holen der Wetterdaten."
         outputstr = "Kein Netzwerk. Die Systemzeit ist " + str(datetime.datetime.now())
         weatherString.append(outputstr)
         assistant_soundprocess.CreateSoundProcessAplay(assistant_soundprocess.CreateVoiceOut(outputstr), 100)
